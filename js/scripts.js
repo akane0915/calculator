9f6e06c0c0b8aa82c220e75c516425e3d2b0caf2
Asia Kane and Spencer Ruiz <asia@>
2017-03-20 10:23:50 -0700

    add subtraction, division, and multiplication functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
+// Define number1 and number2 Code
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"));
+
+// Addition Code
 var add = function(number1,number2){
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+// Subtraction Code
+var subtract = function (number1,number2){
+  return number1 - number2;
+};
 
-alert(add(number1,number2));
+// Multiplication Code
+var multiply = function (number1,number2){
+  return number1 * number2;
+};
+
+// Division Code
+var divide = function (number1,number2){
+  return number1 / number2;
+};
+
+// Call Results Code
+var result = divide(number1,number2);
+
+alert(result);
